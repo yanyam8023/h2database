d34c3878446a7a53faf5fde1565c9ea4f0bd3f84
Andrei Tokar <andrei.tokar@gmail.com>
2019-04-05 19:30:52 -0400

    CursorPos.filterUnsavedPages()

--- h2/src/main/org/h2/mvstore/CursorPos.java
+++ h2/src/main/org/h2/mvstore/CursorPos.java
@@ -6,22 +6,28 @@
 package org.h2.mvstore;
 
 /**
- * A position in a cursor
+ * A position in a cursor.
+ * Instance represents a node in the linked list, which traces path
+ * fom a specific (target) key within a leaf node all the way up to te root
+ * (bottom up path).
  */
 public class CursorPos implements RootReference.VisitablePages
 {
     /**
-     * The current page.
+     * The page at the current level.
      */
     public Page page;
 
     /**
-     * The current index.
+     * Index of the key (within page above) used to go down to a lower level
+     * in case of intermediate nodes, or index of the target key for leaf a node.
+     * In a later case, it could be negative, if the key is not present.
      */
     public int index;
 
     /**
-     * The position in the parent page, if any.
+     * Next node in the linked list, representing the position within parent level,
+     * or null, if we are at the root level already.
      */
     public CursorPos parent;
 
@@ -29,6 +35,27 @@ public class CursorPos implements RootReference.VisitablePages
         this.page = page;
         this.index = index;
         this.parent = parent;
+    }
+
+    CursorPos filterUnsavedPages(MVMap.IntValueHolder unsavedMemoryHolder) {
+        CursorPos head = this;
+        while (head != null && !head.page.isSaved()) {
+            unsavedMemoryHolder.value -= head.page.getMemory();
+            head = head.parent;
+        }
+        if (head != null) {
+            CursorPos tail = head;
+            CursorPos next;
+            while ((next = tail.parent) != null) {
+                if (!next.page.isSaved()) {
+                    tail.parent = next.parent;
+                    unsavedMemoryHolder.value -= next.page.getMemory();
+                } else {
+                    tail = next;
+                }
+            }
+        }
+        return head;
     }
 
     @Override
